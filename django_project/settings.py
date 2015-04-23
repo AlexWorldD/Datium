4fce29a1cf5bc4533770f218649d4dcd4aa0b033
ManOnTheStreet <manonthestreet@hushmail.com>
2015-04-23 21:49:25 +0300

    Updated api for user (GET by username) and created api for documents (not tested yet!)

--- django_project/settings.py
+++ django_project/settings.py
@@ -73,8 +73,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        #'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
